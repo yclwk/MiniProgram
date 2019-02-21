84e2417830b56dc70401c5f8fc5f7299c1ca2450
yclwk <38937128+yclwk@users.noreply.github.com>
2019-02-21 15:18:15 +0800

    创建第一个小程序

---
+++ 第一个小程序/pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/image/IMG_1908.JPG'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  onLoad: function() {
+
+  }
+
+})
